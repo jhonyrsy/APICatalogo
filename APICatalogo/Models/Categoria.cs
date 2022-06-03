8a024c70276c5795ed320d5d9d74dd7b48447bf5
jhonyrsy <=>
2022-06-03 17:37:40 -0300

    Inicio do projeto

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace APICatalogo.Models;
+
+public class Categoria
+{
+    public Categoria()
+    {
+        Produtos = new List<Produto>();
+    }
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+    public ICollection<Produto>?  Produtos { get; set; }
+}
