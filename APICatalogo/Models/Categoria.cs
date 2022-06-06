5846d37f2bfeb756a81cbacf0f4c9352cac37996
jhonyrsy <=>
2022-06-06 17:27:32 -0300

    Utilzando DataAnnotations

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,13 +1,22 @@
-﻿namespace APICatalogo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace APICatalogo.Models;
+
+[Table("Categorias")]
 public class Categoria
 {
     public Categoria()
     {
         Produtos = new List<Produto>();
     }
+    [Key]
     public int CategoriaId { get; set; }
+    [Required]
+    [MaxLength(80)]
     public string? Nome { get; set; }
+    [Required]
+    [MaxLength(300)]
     public string? ImagemUrl { get; set; }
     public ICollection<Produto>?  Produtos { get; set; }
 }
